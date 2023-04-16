1befbce17ea6ee89991f1734edc3374558a57f22
Nadica Miljković <41929315+NadicaSm@users.noreply.github.com>
2023-04-16 15:09:54 +0200

    Update xSQI_calc.m

--- xSQI_calc.m
+++ xSQI_calc.m
@@ -1,9 +1,9 @@
-function xSQI = xSQI_calc(data,annot1,window1,window2)
-    % Code calculate xSQI of signal by channels
-    % xSQI is power ratio of surrounding of annot +- window1
-    % and wider surrounding +- window2
-    borders = border(annot1,window1,length(data));
-    borders_mQRS = border(annot1,window2,length(data));
+function xSQI = xSQI_calc(data, annot1, window1, window2)
+    % Code calculates xSQI of signal by channels
+    % xSQI is power ratio of surrounding of annot 
+    % +/- window1 and wider surrounding +/- window2
+    borders = border(annot1, window1, length(data));
+    borders_mQRS = border(annot1, window2, length(data));
     
     %% xSQI calcultion
 
